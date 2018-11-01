b8b92ac6d9e79837c3000497004939450dc21e70
phaonica <phaonica@gmail.com>
2018-10-31 20:15:26 -0500

    This is what I have after finishing "Nether and End Ores - Minecraft Modding 1.12 - Episode 9.5" @ https://youtu.be/bJSsU6YERZY

--- src/main/java/phaonica/myfirstmod/gen/TutorialOreGen.java
+++ src/main/java/phaonica/myfirstmod/gen/TutorialOreGen.java
@@ -2,7 +2,11 @@ package phaonica.myfirstmod.gen;
 
 import java.util.Random;
 
+import com.google.common.base.Predicate;
+
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.state.pattern.BlockMatcher;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -14,13 +18,23 @@ import phaonica.myfirstmod.init.BlockInit;
 
 public class TutorialOreGen implements IWorldGenerator
 {
-	private WorldGenerator tutorial_ore;
+	private WorldGenerator overworld, nether, end;
 	
 	public TutorialOreGen()
 	{
 		IBlockState blockstate = BlockInit.tutorial_ore.getDefaultState();
 		int vein_size = 9; 
-		tutorial_ore = new WorldGenMinable(blockstate, vein_size);
+		overworld = new WorldGenMinable(blockstate, vein_size);
+		
+		IBlockState blockstate2 = BlockInit.tutorial_ore.getDefaultState();
+		int vein_size2 = 9; 
+		Predicate<IBlockState> blockmatcher2 = BlockMatcher.forBlock(Blocks.NETHERRACK);
+		nether = new WorldGenMinable(blockstate2, vein_size2, blockmatcher2);
+		
+		IBlockState blockstate3 = BlockInit.tutorial_ore.getDefaultState();
+		int vein_size3 = 9;
+		Predicate<IBlockState> blockmatcher3 = BlockMatcher.forBlock(Blocks.END_STONE);
+		end = new WorldGenMinable(blockstate3, vein_size3, blockmatcher3);
 	}
 
 	@Override
@@ -29,11 +43,13 @@ public class TutorialOreGen implements IWorldGenerator
 		switch(world.provider.getDimension())
 		{
 			case 0: // overworld
-				runGenerator(tutorial_ore, world, random, chunkX, chunkZ, 50, 0, 50);
+				runGenerator(overworld, world, random, chunkX, chunkZ, 50, 0, 50);
 				break;
 			case 1: // end
+				runGenerator(end, world, random, chunkX, chunkZ, 50, 0, 255);
 				break;
 			case -1: // nether
+				runGenerator(nether, world, random, chunkX, chunkZ, 50, 0, 50);
 				break;
 		}
 	}
